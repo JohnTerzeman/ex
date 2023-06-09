2fae91bb308fb35abb6d91404edd360ca246b2dc
Джон <knopa.durinda@gmail.com>
2023-06-09 10:39:50 +0300

    firsCommit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,31 @@
+import java.util.Optional;
+
 public class  Main {
     public static void main(String[] a)
     {
-        System.out.printf("0");
+
+        for(int i=0; i<10; i++)
+        {
+            System.out.print(i + " ");
+        }
+        System.out.println(" ");
+
+        int x =4;
+        int y =1;
+        int z=1;
+        while(z<=5)
+        {
+            y=y * x;
+            System.out.println("результат: " + y);
+            z++;
+        }
+        System.out.println(" ");
+
+        int num = 5;
+        do{
+            System.out.println("результат : " + num);
+            num++;
+        }
+        while (num < 10);
     }
 }
